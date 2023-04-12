6dc3a675226e0d784cfc6e9289726053525eb7c2
ZU <zshaihova@gmail.com>
2023-04-12 20:53:18 +0300

    начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        //определяет, чем пользуется клиент (iOS или Android), и выдает соответствующее сообщение:
+        //
+        //Для iOS — «Установите версию приложения для iOS по ссылке».
+        //Для Android — «Установите версию приложения для Android по ссылке».
+        //Объявите переменную clientOS, которая равна 0 или 1 (0 — iOS, 1 — Android).
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+    }
+
+
+}
